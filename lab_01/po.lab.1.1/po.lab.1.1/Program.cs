97f8dcbfef764f83bb99c1afefb2443f3c883df1
Adrian Bazior <Adrian.Bazior@microsoft.wsei.edu.pl>
2022-03-10 09:07:01 +0100

    Pierwsza wersja

---
+++ lab_01/po.lab.1.1/po.lab.1.1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace po.lab._1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
