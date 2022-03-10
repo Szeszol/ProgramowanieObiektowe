7920f9d98c786e8de13de9f8e86bdf99f7cb80c2
Adrian Bazior <Adrian.Bazior@microsoft.wsei.edu.pl>
2022-03-10 09:54:46 +0100

    test

--- lab_01/Po.Labki.1.1/Po.Labki.1.1/Program.cs
+++ lab_01/Po.Labki.1.1/Po.Labki.1.1/Program.cs
@@ -6,7 +6,37 @@ namespace Po.Labki._1._1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           
         }
     }
+    public class Ułamek 
+    {
+        public int Licznik { get; }
+
+        public int Mianownik { get; }
+
+        Ułamek()
+        {
+         
+        }
+
+        Ułamek(int licznik, int mianownik)
+        {
+            this.Mianownik = mianownik;
+            this.Licznik = licznik;
+        }
+
+        Ułamek(Ułamek Ułamek)
+        {
+            this.Licznik = Ułamek.Licznik;
+            this.Mianownik = Ułamek.Mianownik;
+        }
+
+        public override string ToString()
+        {
+            return Licznik.ToString(); + " / " + Mianownik.ToString()
+        }
+       
+    }
 }
+
