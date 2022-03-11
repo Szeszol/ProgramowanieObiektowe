327ad7d84e5cb0be3a5a273f10fe6d3f796f8ec6
Szeszol <101318363+Szeszol@users.noreply.github.com>
2022-03-11 11:27:43 +0100

    Update Program.cs

--- lab_01/Po.Labki.1.1/Po.Labki.1.1/Program.cs
+++ lab_01/Po.Labki.1.1/Po.Labki.1.1/Program.cs
@@ -1,15 +1,32 @@
-﻿using System;
+using System;
+
 
 namespace Po.Labki._1._1
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-           
+            Ułamek[] Ułamki = new Ułamek[]
+            {
+                new Ułamek(4, 7),
+                new Ułamek(13, 17),
+                new Ułamek(25, 100)
+
+
+            };
+            Console.WriteLine("Nie posortowane");
+            for (int i = 0; i < Ułamek.Length; ++i)
+                Console.WriteLine(Ułamki[i]);
+
+            Array.Sort(Ułamek);
+
+            Console.WriteLine("Posortowane po wielkości mianownika");
+            for (int i = 0; i < Ułamek.length ; ++i)
+                Console.WriteLine(Ułamki[i]);
         }
     }
-    public class Ułamek 
+    public class Ułamek: IEquatable<Ułamek>, IComparable<Ułamek>
     {
         public int Licznik { get; }
 
@@ -17,7 +34,7 @@ namespace Po.Labki._1._1
 
         Ułamek()
         {
-         
+
         }
 
         Ułamek(int licznik, int mianownik)
@@ -31,12 +48,30 @@ namespace Po.Labki._1._1
             this.Licznik = Ułamek.Licznik;
             this.Mianownik = Ułamek.Mianownik;
         }
+        public int CompareTo(Ułamek other)
+        {
+            if (other == null) return -1;
+            if (other == this) return 0;
+            {
+                int diff = this.Mianownik - other.Mianownik;
+                if (diff > 0) return +1;
+                if (diff < 0) return -1;
 
+                return 0;
+            }
+
+        }
+        public bool Equals(Ułamek other)
+        {
+            if (other == null) return false;
+            if (other == this) return true;
+
+            return Object.Equals(this.Licznik, other.Licznik) && Object.Equals(this.Mianownik, other.Mianownik);
+        }
         public override string ToString()
         {
-            return Licznik.ToString(); + " / " + Mianownik.ToString()
+            return Licznik.ToString() + " / " + Mianownik.ToString();
         }
-       
+
     }
 }
-
